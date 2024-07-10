345ffd49a37b73fefe6315b751a833e174fa63ac
Divya Rani <91890152+RDivya01@users.noreply.github.com>
2024-07-11 00:34:45 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,1 +1,16 @@
+#ifndef STRING_CALCULATOR_H
+#define STRING_CALCULATOR_H
+
+#include <string>
+#include <vector>
+
+class StringCalculator {
+public:
+    int Add(const std::string& numbers);
+
+private:
+    std::vector<int> ParseNumbers(const std::string& numbers, const std::string& delimiter);
+};
+
+#endif // STRING_CALCULATOR_H
 
