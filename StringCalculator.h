55a78afc2b9a9c1d4cba5a39a879ff499e0b2bfc
Divya Rani <91890152+RDivya01@users.noreply.github.com>
2024-07-13 21:05:59 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -3,14 +3,15 @@
 
 #include <string>
 #include <vector>
+#include <stdexcept>
 
 class StringCalculator {
 public:
     int add(const std::string& numbers);
 
 private:
-    std::vector<int> ParseNumbers(const std::string& numbers, const std::string& delimiter);
+    std::vector<int> parseNumbers(const std::string& numbers, const std::string& delimiter);
+    std::string extractDelimiter(std::string& numbers);
 };
 
 #endif // STRING_CALCULATOR_H
-
